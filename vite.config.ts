a5320a7c304aed4250a0ac6bc07034e988917f29
Maria Fernanda <mariafernandadev@gmail.com>
2025-05-27 20:25:43 -0500

    ga

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,8 +11,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_redirects",
+          dest: "." // lo colocará directamente en dist/
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
